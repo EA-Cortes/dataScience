b04a8c86fe991bb1dd93cfae932e46c40138b701
Alejandro Cortes <acortes@Alejandros-MacBook-Pro.local>
2019-03-04 01:15:22 -0500

    Added more stats

--- us_windTurbine/windTurbs.py
+++ us_windTurbine/windTurbs.py
@@ -18,70 +18,87 @@ df = pd.read_csv("uswtdb_v1_3_20190107.csv")
 wtList = []					# List of wint Turbines per state
 totalWindTurbs = 0			# Count the total wind turbines
 # Filter data by states
-
-
 for state in states:
 	thisState = state + " has "
 	filterDF = df[ df['t_state'] == state]
 	windTurbs = filterDF.size/24
 	wtList.append(windTurbs)
 	totalWindTurbs += windTurbs
-
 	thisState = thisState + str(windTurbs) + " wind turbines"
 	# print(thisState)
 
 avgWT = sum(wtList)/len(wtList)
-
 wtCount = "There are a total of " + str(totalWindTurbs) + " wind turbines in the US!"
 print(wtCount)
-
 
 # ----- # State average stats # ----- # 
 outputAVG = "There are an average of " + str(avgWT) + " wind turbines per state"
 print(outputAVG)
 print("However, only the following states are above average")
 
-aboveAvgStates = 0
 aboveAvgWT = 0
-for state in states:
-	thisState = state + " has "
-	filterDF = df[ df['t_state'] == state]
-	windTurbs = filterDF.size/24
-	
-	wtList.append(windTurbs)
-	#Threshhold
-	if(windTurbs > avgWT):
-		aboveAvgStates += 1
-		aboveAvgWT += windTurbs
-		thisState = thisState + str(windTurbs) + " wind turbines"
-		print(thisState)
-
-percent = 100 * (aboveAvgWT / float(totalWindTurbs))
-percent = (format(percent, '.2f'))
-aboveAvgOut = "There are " + str(aboveAvgStates) + " states that have more than average wind Turbines.\nThey have a combined " + str(aboveAvgWT) + " wind turbines. \nThis means they account for " + str(percent) + "% of the wind turbines"
-print(aboveAvgOut)
-
-print("")
+aboveAvgStates = 0
+sufficientWT = 0
+sufficientStates = 0
 belowAvgWT = 0
 belowAvgStates = 0
+superLowWT = 0
+superLowStates = 0
 for state in states:
 	thisState = state + " has "
 	filterDF = df[ df['t_state'] == state]
 	windTurbs = filterDF.size/24
 	
 	#Threshhold
+	if(windTurbs > 990):
+		sufficientStates += 1
+		sufficientWT += windTurbs
+		thisState = thisState + str(windTurbs) + " wind turbines"
+		print(thisState)
+		if(windTurbs > avgWT):
+			aboveAvgStates += 1
+			aboveAvgWT += windTurbs
+			thisState = thisState + str(windTurbs) + " wind turbines"
+			
+	#Threshhold
 	if(windTurbs < 100):
+		if(windTurbs < 50):
+			superLowStates += 1
+			superLowWT += windTurbs
+			thisState = thisState + str(windTurbs) + " wind turbines"
+			# print(thisState)
+		else:
+			belowAvgStates += 1
+			belowAvgWT += windTurbs
+			thisState = thisState + str(windTurbs) + " wind turbines"
+			# print(thisState)
+	#Threshhold
+	if(windTurbs < 50):
 		belowAvgStates += 1
 		belowAvgWT += windTurbs
 		thisState = thisState + str(windTurbs) + " wind turbines"
-		print(thisState)
+		# print(thisState)
 
+# ----- # Output results # ----- #
+percent = 100 * (aboveAvgWT / float(totalWindTurbs))
+percent = (format(percent, '.2f'))
+aboveAvgOut = "\nThere are " + str(aboveAvgStates) + " states that have more than average wind Turbines.\nThey have a combined " + str(aboveAvgWT) + " wind turbines. \nThis means they account for " + str(percent) + "% of the wind turbines"
+print(aboveAvgOut)
+
+percent = 100 * (sufficientWT / float(totalWindTurbs))
+percent = (format(percent, '.2f'))
+aboveAvgOut = "\nThere are " + str(sufficientStates) + " states that have more 1000 wind Turbines.\nThey have a combined " + str(sufficientWT) + " wind turbines. \nThis means they account for " + str(percent) + "% of the wind turbines"
+print(aboveAvgOut)
 
 percent = 100 * (belowAvgWT / float(totalWindTurbs))
 percent = (format(percent, '.2f'))
-aboveAvgOut = "There are " + str(belowAvgStates) + " states have < 100 wind Turbines.\nThey have a combined " + str(belowAvgWT) + " wind turbines. \nThis means they account for " + str(percent) + "% of the wind turbines"
-print(aboveAvgOut)
+belowAvgOut = "\nThere are " + str(belowAvgStates) + " states have < 100 wind Turbines.\nThey have a combined " + str(belowAvgWT) + " wind turbines. \nThis means they account for " + str(percent) + "% of the wind turbines"
+print(belowAvgOut)
 
+percent = 100 * (superLowWT / float(totalWindTurbs))
+percent = (format(percent, '.2f'))
+superLowOut = "\nThere are " + str(superLowStates) + " states have < 50 wind Turbines.\nThey have a combined " + str(superLowWT) + " wind turbines. \nThis means they account for " + str(percent) + "% of the wind turbines"
+print(superLowOut)
 
 
 # The following may be useful to analyze the data using different parameters
